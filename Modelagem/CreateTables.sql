772536672de98e230510e3fac1bc99487c4279fd
CristianFreitas <cristiantfreitas@gmail.com>
2017-03-24 18:01:33 -0300

    Update CreateTables.sql

--- Modelagem/CreateTables.sql
+++ Modelagem/CreateTables.sql
@@ -1,14 +1,35 @@
 CREATE TABLE roles (
-  	 id_roles INT NOT NULL AUTO_INCREMENT,
- 	 roles varchar(45) not null,
-	 PRIMARY KEY(id_roles)
+  id_roles INT NOT NULL AUTO_INCREMENT,
+  roles varchar(45) not null,
+  PRIMARY KEY(id_roles)
+);
+
+CREATE TABLE status_products(
+	id_status int not null AUTO_INCREMENT,
+	status_prod int(1) not null,
+	qtd_products varchar(200) not null,
+	id_products int not null,
+	PRIMARY KEY	(id_status)
+);
+
+CREATE TABLE users (
+  id_users INT NOT NULL AUTO_INCREMENT,
+  name varchar(100) NOT NULL,
+  email varchar(100) not null,
+  username varchar(45) not null,
+  password char(128) not null,
+  salt char(128) not null,
+  role_id int not null,
+  status_user int(1) not null,
+  PRIMARY KEY(id_users),
+  FOREIGN KEY (role_id) REFERENCES roles(role_id)
 );
 
 CREATE TABLE login_attempts (
 	id_attempts int not null AUTO_INCREMENT,
 	id_users int not null,
 	created_at TIMESTAMP,
-	PRIMARY KEY (id_attempts),
+	PRIMARY KEY	(id_attempts),
  	FOREIGN KEY (id_users) REFERENCES users(id_users)
 );
 
@@ -16,20 +37,11 @@ CREATE TABLE passwords (
 	id_passwords int not null AUTO_INCREMENT,
 	id_users int(1) not null,
 	token varchar(255) not null,
-	status int(1) not null DEFAULT 0,
-	PRIMARY KEY (id_passwords),
+	status int(1) not null,
+	PRIMARY KEY	(id_passwords),
  	FOREIGN KEY (id_users) REFERENCES users(id_users)
 );
 
-
-CREATE TABLE status_products(
-	id_status int not null AUTO_INCREMENT,
-	status int(1) not null,
-	qtd_products int(200) not null,
-	id_products int not null,
-	PRIMARY KEY (id_status),
- 	FOREIGN KEY (id_products) REFERENCES products(id_products)
-)
 
 CREATE TABLE products (
 	id_products int not null AUTO_INCREMENT,
@@ -37,23 +49,12 @@ CREATE TABLE products (
 	description varchar(200),
 	brands varchar(45) not null,
 	tipo varchar(45) not null,
-	value DOUBLE(8, 3) not null,
+	value float(8, 3) not null,
 	id_users int not null,
+	id_status int not null,
  	PRIMARY KEY(id_products),
- 	FOREIGN KEY (id_users) REFERENCES users(id_users)
+ 	FOREIGN KEY (id_users) REFERENCES users(id_users),
+ 	FOREIGN KEY (id_status) REFERENCES status_products(id_status)
 );
 
-
-CREATE TABLE users (
-	 id_users INT NOT NULL AUTO_INCREMENT,
- 	 name varchar(100) NOT NULL,
-	 email varchar(100) not null,
-	 username varchar(45) not null,
-	 password char(128) not null,
-	 salt char(128) not null,
-	 role_id int not null,
-	 status int(1) not null DEFAULT 1,
-	 PRIMARY KEY(id_users),
- 	 FOREIGN KEY (role_id) REFERENCES roles(role_id)
-);
 
